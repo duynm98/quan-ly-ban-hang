94593ec2807c46899b838e76e58bf0c190b3065a
nguyenphonglong <longnpk61hust@gmail.com>
2019-12-23 20:46:15 +0700

    Add Order Acivity xml

---
+++ app/src/main/java/com/duynm/qlbanhang/ui/addorder/AddOrderActivity.java
@@ -0,0 +1,16 @@
+package com.duynm.qlbanhang.ui.addorder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.duynm.qlbanhang.R;
+
+public class AddOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_order);
+    }
+}
