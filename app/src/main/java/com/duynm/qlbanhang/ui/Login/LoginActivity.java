23851e16c37db56b78c76ac7f8fd0e89b7d74a33
duy07t1k23cht <duynguyengb1998@gmail.com>
2019-12-24 00:07:32 +0700

    Man hinh dang nhap

--- app/src/main/java/com/duynm/qlbanhang/ui/Login/LoginActivity.java
+++ app/src/main/java/com/duynm/qlbanhang/ui/Login/LoginActivity.java
@@ -2,36 +2,68 @@ package com.duynm.qlbanhang.ui.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.duynm.qlbanhang.R;
+import com.duynm.qlbanhang.ui.home.MainActivity;
 
-public class LoginActivity extends AppCompatActivity {
-    TextView txtSignUp;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final static String DEFAULT_USERNAME = "admin";
+    private final static String DEFAULT_PASSWORD = "admin";
+
+    private TextView txtSignUp;
+    private EditText txtAccount, txtPassword;
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
         initViewComponent();
         initUI();
-        
+
     }
 
     private void initUI() {
-    }
-
-    private void initViewComponent() {
-        txtSignUp=(TextView) findViewById(R.id.txtSignUp); 
         addLineForSignUp();
     }
 
+    private void initViewComponent() {
+        txtSignUp = (TextView) findViewById(R.id.txtSignUp);
+        txtAccount = findViewById(R.id.txtAcount);
+        txtPassword = findViewById(R.id.txtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(this);
+        txtSignUp.setOnClickListener(this);
+    }
+
     private void addLineForSignUp() {
-        String mystring=new String("Đăng kí");
+        String mystring = "Đăng kí";
         SpannableString content = new SpannableString(mystring);
         content.setSpan(new UnderlineSpan(), 0, mystring.length(), 0);
         txtSignUp.setText(content);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnLogin:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.txtSignUp:
+                Toast.makeText(this, "Chưa có chức năng đăng ký", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
